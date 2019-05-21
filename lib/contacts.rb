a1ca41571bcffbe93d052058e5633537f0dead96
Margot Konig <margot.konig@blackrock.com>
2019-05-21 16:51:05 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -19,9 +19,6 @@ contacts.each do |name,data|
   data.each do |key, value|
     if key == :favorite_ice_cream_flavors
       value.delete_if {|ice_cream| ice_cream == "strawberry"}
-      
-  value.delete_if do |ice cream|
-    ice_cream == "strawberry"
     end
   end
 end
